fca076c7c786ab875caed0b3b1ac06fc734afc35
Pedro Paccola <paccolapedro@gmail.com>
2022-11-08 19:49:44 -0300

    end of EP02

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pedropaccola/go-bank-anthdm
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.7
+)
--- types.go
+++ types.go
@@ -1,20 +1,29 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
-type Account struct {
-	ID        int    `json:"id"`
+type CreateAccountRequest struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
-	Number    int64  `json:"number"`
-	Balance   int64  `json:"balance"`
+}
+
+type Account struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 func NewAccount(FirstName, LastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(1000) + 1,
 		FirstName: FirstName,
 		LastName:  LastName,
 		Number:    int64(rand.Intn(1000000) + 1),
+		CreatedAt: time.Now().UTC(),
 	}
 }
